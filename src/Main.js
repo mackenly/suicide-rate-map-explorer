e8cb97ef2cf99e172db9404efec12191b6317a6f
Mackenly Jones <53151058+mackenly@users.noreply.github.com>
2022-10-18 23:42:00 -0400

    Legend overhall

--- src/Main.js
+++ src/Main.js
@@ -6,61 +6,72 @@ import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Main() {
-    const { id } = useParams();
-    const queryParameters = new URLSearchParams(window.location.search);
-    const n = queryParameters.get('n') || 42;
-    const w = queryParameters.get('w') || -100;
-    const zoom = queryParameters.get('zoom') || 3;
+	const { id } = useParams();
+	const queryParameters = new URLSearchParams(window.location.search);
+	const n = queryParameters.get('n') || 42;
+	const w = queryParameters.get('w') || -100;
+	const zoom = queryParameters.get('zoom') || 3;
 	const [showSources, setShowSources] = useState(false);
 	const [selectedFips, setSelectedFips] = useState([]);
-    const [mapBoxData, setMapBoxData] = useState(() => {
-        if (n & w & zoom) {
-            return { center: { lon: w, lat: n }, zoom: zoom, style: 'open-street-map' };
-        }
-        return { center: { lon: -100, lat: 42 }, zoom: 3, style: 'open-street-map' };
-    });
+	const [mapBoxData, setMapBoxData] = useState(() => {
+		if (n & w & zoom) {
+			return { center: { lon: w, lat: n }, zoom: zoom, style: 'open-street-map' };
+		}
+		return { center: { lon: -100, lat: 42 }, zoom: 3, style: 'open-street-map' };
+	});
 	const [data, setData] = useState(() => {
-        if (id) {
-            if (selectedFips.length === 0) {
+		if (id) {
+			if (selectedFips.length === 0) {
 				const filteredData = Object.keys(cdcData.counties).reduce((obj, key) => {
 					if (key.startsWith(id)) {
-                        obj[key] = cdcData.counties[key];
-                    }
-                    return obj;
-                }, {});
+						obj[key] = cdcData.counties[key];
+					}
+					return obj;
+				}, {});
 
 				const filter = filteredData;
 				return { counties: filter };
 			} else {
 				return cdcData;
 			}
-        } else {
+		} else {
 			return cdcData;
 		}
-    });
+	});
 
 	function roundTo4(num) {
 		return Math.round(num * 10000) / 10000;
 	}
-    const clickCounty = (event) => {
-        console.log(event);
-        var w = event.points[0].ct[0], n = event.points[0].ct[1], zoom = 5;
-        const url = `/state/${event.points[0].location.substring(
-			0,
-			2
-		)}?n=${n}&w=${w}&zoom=${zoom}`;
-        window.open(url, '_self');
-    };
+	const clickCounty = (event) => {
+		console.log(event);
+		var w = event.points[0].ct[0],
+			n = event.points[0].ct[1],
+			zoom = 5;
+		const url = `/state/${event.points[0].location.substring(0, 2)}?n=${n}&w=${w}&zoom=${zoom}`;
+		window.open(url, '_self');
+	};
 
+	function maxRate(data) {
+        let max = 0;
+		Object.values(data.counties).map((value) => {
+            if (max < (value.suicideData.deaths / value.suicideData.population) * 100000) {
+                max = (value.suicideData.deaths / value.suicideData.population) * 100000;
+            }
+		});
+        return max;
+	}
 
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h1>2020 Suicide Data Map Test</h1>
 				<p>This map illustrates the number of people who died by suicide across the counties of the United States.</p>
-				<a href='/' value="0">
-					View All States
-				</a> Click in a state to see that state's data isolated.
+				<p>
+					<a href="/" value="0">
+						View All States
+					</a>{' '}
+					Click in a state to see that state's data isolated.
+				</p>
 			</header>
 			<div id="map">
 				{
@@ -100,13 +111,7 @@ function Main() {
 										Number of Suicide Deaths: <span style="color:red;">${value.suicideData.deaths === 0 ? 'less than 10' : value.suicideData.deaths}</span><br>
 										No Religious Attendance: <span style="color:red;">${roundTo4(100 - value.religionData.attendanceRate * 100)}%</span></b><extra></extra>`;
 								}),
-								colorbar: {
-									orientation: 'v',
-									title: {
-										text: '(2020 Suicide Deaths / Population) * 100000',
-										side: 'right',
-									},
-								},
+								showscale: false,
 							},
 						]}
 						layout={{
@@ -121,9 +126,18 @@ function Main() {
 								pad: 0,
 							},
 						}}
-                        onClick={clickCounty}
+						onClick={clickCounty}
 					/>
 				}
+			</div>
+			<div id="legend">
+				<p>(2020 Suicide Deaths / Population) * 100000</p>
+				<span>{Math.round(maxRate(data))}</span>
+				<span>{Math.round((maxRate(data) / 2 / 2) * 3)}</span>
+				<span>{Math.round(maxRate(data) / 2)}</span>
+				<span>{Math.round(maxRate(data) / 2 / 2)}</span>
+				<span>0</span>
+				<hr />
 			</div>
 			{!showSources && (
 				<button
